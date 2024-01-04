e7ad01b4d9fb343b0b8762a0c9d472cc123dbea1
Ian Czerkis <czerkisi@msoe.edu>
2024-01-04 17:15:09 -0500

    mobile nav bar finish

--- src/components/MobileNavBar/MobileNavBar.tsx
+++ src/components/MobileNavBar/MobileNavBar.tsx
@@ -1,20 +1,25 @@
-import React, {useEffect, useState} from "react";
-import {useAppSelector} from "../../redux/hooks";
+import React, { useEffect, useState } from "react";
+import { useAppSelector } from "../../redux/hooks";
 import './MobileNavBar.css';
-import {getFile} from "../../App";
-import {Page} from "../../redux/slices/pages";
-import {useNavigate} from "react-router-dom";
+import { getFile } from "../../App";
+import { Page } from "../../redux/slices/pages";
+import { useNavigate } from "react-router-dom";
 
-export default function MobileNavBar(){
+export default function MobileNavBar() {
     const pages = useAppSelector(state => state.pages.pages);
     const [allPages, setAllPages] = useState<Page[]>([]);
+    const [showPages, setShowPages] = useState(false);
     const navigate = useNavigate();
     const aboutPage: Page = {
-        children: [], relativeLink: "/more/about", shortTitle: "About Me"
-    }
+        children: [],
+        relativeLink: "/more/about",
+        shortTitle: "About Me"
+    };
     const contactPage: Page = {
-        children: [], relativeLink: "/more/contact", shortTitle: "Contact"
-    }
+        children: [],
+        relativeLink: "/more/contact",
+        shortTitle: "Contact"
+    };
 
     useEffect(() => {
         const tempPages = pages.slice();
@@ -23,13 +28,46 @@ export default function MobileNavBar(){
         setAllPages(tempPages);
     }, [pages]);
 
-    function redirectHome(){
+    function redirectHome() {
         navigate('/');
     }
+
+    function handleHamburgerClick() {
+        setShowPages(!showPages);
+    }
+
+    function handlePageClick(page: Page) {
+        if (page.children.length === 0){
+            navigate(page.relativeLink);
+            setShowPages(false);
+        } else {
+            setAllPages(page.children);
+        }
+    }
+
     return (
         <div className={'mobile-nav-bar'}>
-            <img className={'mobile-logo-image'} src={getFile('logo.png')} alt={'logo'} onClick={redirectHome}/>
-            <img src={getFile('hamburgerIcon.png')} alt={'nav'} className={'mobile-hamburger-icon'}/>
+            <img className={'mobile-logo-image'} src={getFile('logo.png')} alt={'logo'} onClick={redirectHome} />
+            <span className={'mobile-name-title'}>Ryleigh Leon</span>
+            <img src={getFile('hamburgerIcon.png')} alt={'nav'} className={'mobile-hamburger-icon'} onClick={handleHamburgerClick} />
+
+            {showPages && (
+                <div className={'mobile-pages-list'}>
+                    <span className={'mobile-close-button'} onClick={() => setShowPages(false)}>X</span>
+                    <ul>
+                        {allPages.map((page, index) => (
+                            <li key={index} onClick={() => handlePageClick(page)}>
+                                {page.children.length > 0 ? (
+                                    <span>{page.shortTitle}</span>
+                                ) : (
+                                    <a href={page.relativeLink}>{page.shortTitle}</a>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
-    )
+    );
+
 }

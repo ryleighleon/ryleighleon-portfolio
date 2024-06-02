334f7fd9558cecb4f328c9e53cddf9b948648ae4
Ian Czerkis <czerkisi@msoe.edu>
2024-06-01 21:38:02 -0400

    no hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import './variables.css';
 import NavBar from "./components/NavBar/NavBar";
